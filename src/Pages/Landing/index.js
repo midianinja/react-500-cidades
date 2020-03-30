ed4f541325e592a1f748668af0c55db54635632c
mokasousa <moka.sousa@gmail.com>
2020-03-30 17:53:14 -0300

    landing page landing-container styles

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Button from '../../components/Button'
+import backgroundImg from '../../assets/mergulho-500cidades.png'
 import './styles.css';
 
-export default function Landing() {
+const Landing = () => {
 
     return (
         <div className="landing-container">
             <section className="landing">
+                <img className="background-image" src={backgroundImg} alt="Mergulho 500 Cidades"/>
                 <h2 className="heading-secondary">
                     <span className="heading-secondary--white">Coloque</span>
                     <span className="heading-secondary--green">sua quebrada</span>
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles.css';
 
-export const Button = ({
+const Button = ({
     children,
     type,
     onClick,
@@ -13,3 +13,5 @@ export const Button = ({
         </button>
     );
 }
+
+export default Button;
