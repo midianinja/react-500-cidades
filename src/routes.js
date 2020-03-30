483b10490dbe7ddb00dfe5fee377387c97cf4578
solangealmn <solangealmn@gmail.com>
2020-03-30 00:11:50 -0300

    frontend iniciado.

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Landing from './pages/Landing/index';
+import Register from './pages/Register/index';
+import Profile from './pages/Profile/index';
+import UserList from './pages/UserList/index';
+import UserMap from './pages/UserMap/index';
+
+export default function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/userlist" component={UserList} />
+                <Route path="/usermap" component={UserMap} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
