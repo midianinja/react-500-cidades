92048f50ef2689185e327c5b067c5ac11c10c816
solangealmn <solangealmn@gmail.com>
2020-03-30 22:19:48 -0300

    import de fonts e ativação de rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
-//import Routes from './routes';
+import Routes from './routes';
 
 import './global.css';
 
 function App() {
 
   return (
-    //<Routes />
-    <h1>500 Cidades</h1>
+    <Routes /> 
   );
 }
 
---
+++ src/Pages/Landing/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+
+export default function Landing(){
+    return(
+        <h1>500 Cidades</h1>
+    );
+}
