7fded9af9edfda789975061d4735ee3cff7128b5
mokasousa <moka.sousa@gmail.com>
2020-04-06 18:51:27 -0300

    install apollo and add state variables to inputs

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Routes from './routes';
 import { BrowserRouter} from 'react-router-dom';
+// import ApolloClient from 'apollo-boost';
+// import { ApolloProvider } from '@apollo/react-hooks';
 import './global.css';
+
+// const client = new ApolloClient({
+//   uri:'',
+// });
 
 function App() {
   console.log(process.env.REACT_APP_API_KEY);
-
   return (
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
   );
 }
 
--- src/Pages/Register/index.js
+++ src/Pages/Register/index.js
@@ -1,12 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 //import { Link } from 'react-router-dom';
+// import gql from 'graphql-tag'
+//import { gql } from 'apollo-boost';
+// import { useQuery } from 'react-apollo'
 import Button from '../../components/Button'
 import Input from '../../components/Input'
 import './styles.css';
 
+// const REGISTER_USER = gql`
+//     mutation user?? (
+//      $name: String
+//      ) { }
+//      {tipology, name, job, profile_image, cover_image, biography, skills, email, phone, instagram, facebook,
+//      site_address, birth_date, gender, sexual_orientation, race}
+
 const Register = () => {
-    // const [name, useName] = useState('Seu Nome');
-    // const [job, useJob] = useState('Profissão');
+    // const {loading, data: {putUser: user} } = useQuery(PUT_USER_QUERY) 
+    const [userInfo, setUserInfo] = useState({
+        tipology: '', //?
+        name: 'Seu nome',
+        job: 'Profissão',
+        profile_image: '',
+        cover_image: '',
+        biography: '',
+        skills: [],//tags
+        email: '',
+        phone: '',
+        instagram: '',
+        facebook: '',
+        site_address: '',
+        birth_date: '',
+        gender: '',
+        sexual_orientation: '',
+        race: '',
+    })
+    const [addressInfo, setAddressInfo] = useState({
+        street: '',
+        number: '',
+        complement: '',
+        district: '',
+        city: '',
+        zipcode: '',
+        state: '',
+        country: '',
+        place_id: '',
+        geometry: '',
+        latitude: '',
+        longitude: '',
+    })
+
+    const onChangeUserInfo = (e) => {
+        setUserInfo({...userInfo, [e.target.name]: e.target.value})
+    }
+
+    const onChangeAddressInfo = (e) => {
+        setAddressInfo({...addressInfo, [e.target.name]: e.target.value})
+    }
+
+    // const onSubmit = (e) => {
+    //     e.preventDefault();
+    // }
+
     return (
         <div className="register-container">
             <section className="navigation">
@@ -18,8 +72,8 @@ const Register = () => {
                     {/* icon */}
                 </div>
                 <div className="register-text">
-                    <h1 className="add-name" contentEditable="false">Seu nome</h1>  {/*<icon onClick={this.contenteditable = "true"}>*/}
-                    <h2 className="add-job" contentEditable="false">Profissão</h2>  {/*<icon onClick={this.contenteditable = "true"}>*/}
+                    <h1 className="add-name" contentEditable="false">{userInfo.name}</h1>  {/*<icon onClick={this.contenteditable = "true"}>*/}
+                    <h2 className="add-job" contentEditable="false">{userInfo.job}</h2>  {/*<icon onClick={this.contenteditable = "true"}>*/}
                 </div>
             </section>
             <form className="register-form">
@@ -27,7 +81,13 @@ const Register = () => {
                     <h3 className="heading-terciary">
                         Escreva uma biografia
                     </h3>
-                    <textarea id="biography" placeholder="Digite aqui sua biografia" maxLength="250"></textarea>
+                    <textarea
+                        id="biography"
+                        placeholder="Digite aqui sua biografia"
+                        maxLength="250"
+
+                    >
+                    </textarea>
                 </div>
                 <div>
                     <h3 className="heading-terciary">
@@ -39,6 +99,9 @@ const Register = () => {
                         id="skills"
                         classLabel="register-label"
                         children="Adicione suas tags"
+                        name="skills"
+                        value={userInfo.skills}
+                        onChange={onChangeUserInfo}
                     />
                 </div>
                 <div>
@@ -52,6 +115,9 @@ const Register = () => {
                             id="email"
                             classLabel="register-label"
                             children="E-mail"
+                            name="email"
+                            value={userInfo.email}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="tel"
@@ -59,6 +125,9 @@ const Register = () => {
                             id="phone"
                             classLabel="register-label"
                             children="Telefone"
+                            name="phone"
+                            value={userInfo.phone}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="url"
@@ -66,6 +135,9 @@ const Register = () => {
                             id="instagram"
                             classLabel="register-label"
                             children="Instagram"
+                            name="instagram"
+                            value={userInfo.instagram}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="url"
@@ -73,6 +145,9 @@ const Register = () => {
                             id="facebook"
                             classLabel="register-label"
                             children="Facebook"
+                            name="facebook"
+                            value={userInfo.facebook}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="url"
@@ -80,6 +155,9 @@ const Register = () => {
                             id="site_address"
                             classLabel="register-label"
                             children="Site/portifolio"
+                            name="site_address"
+                            value={userInfo.site_address}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="date"
@@ -87,13 +165,19 @@ const Register = () => {
                             id="birth-date"
                             classLabel="register-label"
                             children="Data de nascimento"
+                            name="birth_date"
+                            value={userInfo.birth_date}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="text"
                             classInput="register-input"
                             id="gender"
                             classLabel="register-label"
-                            children="gênero"
+                            children="Gênero"
+                            name="gender"
+                            value={userInfo.gender}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="text"
@@ -101,6 +185,9 @@ const Register = () => {
                             id="sexual-orientation"
                             classLabel="register-label"
                             children="Orientação sexual"
+                            name="sexual_orientation"
+                            value={userInfo.sexual_orientation}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="text"
@@ -108,6 +195,9 @@ const Register = () => {
                             id="race"
                             classLabel="register-label"
                             children="Raça/cor"
+                            name="race"
+                            value={userInfo.race}
+                            onChange={onChangeUserInfo}
                         />
                         <Input
                             type="text"
@@ -115,6 +205,9 @@ const Register = () => {
                             id="zipcode"
                             classLabel="register-label"
                             children="CEP"
+                            name="zipcode"
+                            value={addressInfo.zipcode}
+                            onChange={onChangeAddressInfo}
                         />
                         <Input
                             type="text"
@@ -136,6 +229,9 @@ const Register = () => {
                             id="city"
                             classLabel="register-label"
                             children="Cidade"
+                            name="city"
+                            value={addressInfo.city}
+                            onChange={onChangeAddressInfo}
                         />
                         <Input
                             type="text"
@@ -143,11 +239,15 @@ const Register = () => {
                             id="country"
                             classLabel="register-label"
                             children="País"
+                            name="country"
+                            value={addressInfo.country}
+                            onChange={onChangeAddressInfo}
                         />
                     </div>
                 </div>
                 <div className="register-submit">
                     <Button
+                        type="submit"
                         children="Criar conta"
                         className="btn--blue"
                     />
