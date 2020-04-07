fc8c86605712a64ce1c11e68185255bc7b5fcb3c
solangealmn <solangealmn@gmail.com>
2020-04-07 01:10:49 -0300

    feat: Menu e responsividade concluídos.

--- src/Pages/Landing/index.js
+++ src/Pages/Landing/index.js
@@ -21,106 +21,176 @@ import './styles.css';
 
 const Landing = () => {
     return (
-        <div className="landing-container">
-            <Menu>
-                <Routes/>
-            </Menu>
-            <section className="landing">
-                <div className="landing-top">
-                    <div className="landing-logo">
-                        <img className="logo-img" src={logoImg} alt="Logotipo 500 Cidades"/>
-                    </div>
-                    <div className="landing-title">
-                        <h1 className="heading-primary">
-                            <span className="heading-primary--white">Coloque</span>
-                            <span className="heading-primary--green">sua quebrada</span>
-                            <span className="heading-primary--white">no mapa</span>
-                        </h1>
-                        <div className="landing-top-links">
-                            <Button className="btn--red">Quero!</Button>
-                            <Link href="#" alt="Já tenho cadastro" className="anchor-link">Já tenho cadastro.</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="landing-bottom">
-                    <h2 className="heading-secondary--landing-bottom">
-                        Um mergulho no Brasil de verdade
-                    </h2>
-                    <p className="paragraph--landing-bottom">
-                        Com o projeto 500 Cidades viajamos pelo Brasil para colocar no mapa pessoas e projetos que criam uma ponte entre a arte e a cultura, e transformam seus espaços de alguma forma, na beleza das pequenas atitudes e nas vitórias diárias.
-                    </p>
-                </div>
-            </section>
-            <section className="activist-map">
-                <div style={{textAlign:"left"}}>
-                    <h2 className="heading-secondary">Mapa Ativista</h2>
-                    <img className="map-img" src={mapImg} alt="Mapa Ativista" />
-                </div>
-                <Button className="btn--blue">Navegue e descubra mais</Button>
-                <div className="steps">
-                    <div>
-                        <img className="steps-img" src={stepImg1} alt="Cadastre-se" />
-                        <p>Cadastre-se</p>
-                    </div>
-                    <div>
-                        <img className="steps-img" src={stepImg2} alt="Coloque sua cidade" />   
-                        <p>Coloque sua cidade no mapa</p>
-                    </div>
-                    <div>
-                        <img className="steps-img" src={stepImg3} alt="Escreva sua história" />
-                        <p>Conte sua história <span>(em breve)</span></p>
-                    </div>
-                </div>
-            </section>
-            <section className="about">
-                <div>
-                    {/* <Container> */}
-                    <h2 className="heading-secondary--about">
-                        500 cidades
-                    </h2>
-                    <div className="paragraph-about">
-                        <p>O 500 cidades é um projeto organizado pela rede de coletivos culturais Fora do Eixo e a rede de mídia livre Ninja e tem por objetivo identificar pessoas, projetos e espaços espalhados pelo Brasil para construir uma rede de territórios ativistas e promover a criação de redes de confiança, empatia e solidariedade, a partir de experiências e histórias de vida.</p>
-                        <p>Enquanto uma onda de intolerância e conservadorismo ganha força, milhares de experiências e práticas colaborativas de superação pessoal e coletiva emergem transformando todo o país num grande laboratório de uma nova cidadania no século XXI. Participar da vida social e política é uma das principais atitudes que caracterizam o brasileiro do século XXI, principalmente depois das jornadas de junho de 2013.</p>
-                        <p>Segundo especialistas estamos diante da maior crise humanitária do último século. Não bastasse tudo isso, o Brasil tem neste momento um presidente incapaz de lidar com os desafios que temos pela frente. Estes dois fatores dão a dimensão da EMERGÊNCIA deste chamado a participação.</p>
-                        <p>Temos que aproveitar este momento para se abrir ainda mais e ampliar as conexões entre as pessoas dispostas a construir em seus territórios esta rede de confiança. Debaixo pra cima, a partir de cada local, chegou a hora de semearmos um novo projeto de sociedade. Mais justo e igualitário. Que empodere e dê visibilidade a toda potência de um Brasil profundo e que precisa emergir.</p>
-                        <p>Venha fazer parte desta história...</p>
-                        </div>
-                    {/* </Container> */}
-                </div>
-            </section>
-            <section className="newsletter">
-                <img className="newsletter-img" src={newsletterImg} alt="Newsletter" />
-                <div className="newsletter-text">
-                    <h3 className="heading-terciary--newsletter">Saiba tudo que está rolando no 500 cidades</h3>
-                    <p>Assine nossa newsletter</p>
-                    <div className="newsletter-subscription">
-                        <Input
-                        type="e-mail"
-                        classInput="newsletter-email"
-                        placeholder="Insira aqui seu e-mail"
-                        id="newsletterEmail"
-                        classLabel="label-email"
-                        />
-                        <Button className="newsletter-btn--blue">></Button>
-                    </div>
-                </div>
-            </section>
-            <section className="footer">
-                <div className="org">
-                    <p>Quem constrói isso com a gente</p>
-                    <div className="org-logos">
-                        <img className="org-logo" src={logoNinjaImg} alt="Logo Mídia Ninja" />
-                        <img className="org-logo" src={logoCircleImg} alt="pesquisar....." />
-                        <img className="org-logo" src={logoAmzImg} alt="Logo Amazônia.org" />
-                    </div>
-                </div>
-                <div className="more-about">
-                    <a className="more-about-link" href="#sobre">Sobre</a>
-                    <a className="more-about-link" href="#politica">Política de privacidade</a>
-                    <a className="more-about-link" href="#termos">Termos de uso</a>
-                </div>
-            </section>
-        </div>
+      <div className="landing-container">
+        <section className="landing">
+          <Menu>
+            <Routes />
+          </Menu>
+          <div className="landing-top">
+            <div className="landing-logo">
+              <img
+                className="logo-img"
+                src={logoImg}
+                alt="Logotipo 500 Cidades"
+              />
+            </div>
+            <div className="landing-title">
+              <h1 className="heading-primary">
+                <span className="heading-primary--white">Coloque</span>
+                <span className="heading-primary--green">sua quebrada</span>
+                <span className="heading-primary--white">no mapa</span>
+              </h1>
+              <div className="landing-top-links">
+                <Button className="btn--red">Quero!</Button>
+                <Link href="#" alt="Já tenho cadastro" className="anchor-link">
+                  Já tenho cadastro.
+                </Link>
+              </div>
+            </div>
+          </div>
+          <div className="landing-bottom">
+            <h2 className="heading-secondary--landing-bottom">
+              Um mergulho no Brasil de verdade
+            </h2>
+            <p className="paragraph--landing-bottom">
+              Com o projeto 500 Cidades viajamos pelo Brasil para colocar no
+              mapa pessoas e projetos que criam uma ponte entre a arte e a
+              cultura, e transformam seus espaços de alguma forma, na beleza das
+              pequenas atitudes e nas vitórias diárias.
+            </p>
+          </div>
+        </section>
+        <section className="activist-map">
+          <div style={{ textAlign: "left" }}>
+            <h2 className="heading-secondary">Mapa Ativista</h2>
+            <img className="map-img" src={mapImg} alt="Mapa Ativista" />
+          </div>
+          <Button className="btn--blue">Navegue e descubra mais</Button>
+          <div className="steps">
+            <div>
+              <img className="steps-img" src={stepImg1} alt="Cadastre-se" />
+              <p>Cadastre-se</p>
+            </div>
+            <div>
+              <img
+                className="steps-img"
+                src={stepImg2}
+                alt="Coloque sua cidade"
+              />
+              <p>Coloque sua cidade no mapa</p>
+            </div>
+            <div>
+              <img
+                className="steps-img"
+                src={stepImg3}
+                alt="Escreva sua história"
+              />
+              <p>
+                Conte sua história <span>(em breve)</span>
+              </p>
+            </div>
+          </div>
+        </section>
+        <section className="about">
+          <div>
+            {/* <Container> */}
+            <h2 className="heading-secondary--about">500 cidades</h2>
+            <div className="paragraph-about">
+              <p>
+                O 500 cidades é um projeto organizado pela rede de coletivos
+                culturais Fora do Eixo e a rede de mídia livre Ninja e tem por
+                objetivo identificar pessoas, projetos e espaços espalhados pelo
+                Brasil para construir uma rede de territórios ativistas e
+                promover a criação de redes de confiança, empatia e
+                solidariedade, a partir de experiências e histórias de vida.
+              </p>
+              <p>
+                Enquanto uma onda de intolerância e conservadorismo ganha força,
+                milhares de experiências e práticas colaborativas de superação
+                pessoal e coletiva emergem transformando todo o país num grande
+                laboratório de uma nova cidadania no século XXI. Participar da
+                vida social e política é uma das principais atitudes que
+                caracterizam o brasileiro do século XXI, principalmente depois
+                das jornadas de junho de 2013.
+              </p>
+              <p>
+                Segundo especialistas estamos diante da maior crise humanitária
+                do último século. Não bastasse tudo isso, o Brasil tem neste
+                momento um presidente incapaz de lidar com os desafios que temos
+                pela frente. Estes dois fatores dão a dimensão da EMERGÊNCIA
+                deste chamado a participação.
+              </p>
+              <p>
+                Temos que aproveitar este momento para se abrir ainda mais e
+                ampliar as conexões entre as pessoas dispostas a construir em
+                seus territórios esta rede de confiança. Debaixo pra cima, a
+                partir de cada local, chegou a hora de semearmos um novo projeto
+                de sociedade. Mais justo e igualitário. Que empodere e dê
+                visibilidade a toda potência de um Brasil profundo e que precisa
+                emergir.
+              </p>
+              <p>Venha fazer parte desta história...</p>
+            </div>
+            {/* </Container> */}
+          </div>
+        </section>
+        <section className="newsletter">
+          <img
+            className="newsletter-img"
+            src={newsletterImg}
+            alt="Newsletter"
+          />
+          <div className="newsletter-text">
+            <h3 className="heading-terciary--newsletter">
+              Saiba tudo que está rolando no 500 cidades
+            </h3>
+            <p>Assine nossa newsletter</p>
+            <div className="newsletter-subscription">
+              <Input
+                type="e-mail"
+                classInput="newsletter-email"
+                placeholder="Insira aqui seu e-mail"
+                id="newsletterEmail"
+                classLabel="label-email"
+              />
+              <Button className="newsletter-btn--blue">></Button>
+            </div>
+          </div>
+        </section>
+        <section className="footer">
+          <div className="org">
+            <p>Quem constrói isso com a gente</p>
+            <div className="org-logos">
+              <img
+                className="org-logo"
+                src={logoNinjaImg}
+                alt="Logo Mídia Ninja"
+              />
+              <img
+                className="org-logo"
+                src={logoCircleImg}
+                alt="pesquisar....."
+              />
+              <img
+                className="org-logo"
+                src={logoAmzImg}
+                alt="Logo Amazônia.org"
+              />
+            </div>
+          </div>
+          <div className="more-about">
+            <a className="more-about-link" href="#sobre">
+              Sobre
+            </a>
+            <a className="more-about-link" href="#politica">
+              Política de privacidade
+            </a>
+            <a className="more-about-link" href="#termos">
+              Termos de uso
+            </a>
+          </div>
+        </section>
+      </div>
     );
 }
 
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,38 +1,66 @@
 import React from 'react';
 import { NavLink } from "react-router-dom";
+import Popup from "reactjs-popup";
+
+import BurgerIcon from '../BurgerIcon';
 
 import './styles.css';
 
 const Menu = ({ close })  => {
+const containerModal = {
+  border: "none",
+  position: 'relative',
+  background: 'rgb(59, 39, 55)',
+  width: '50%',
+};
     return (
-        <header>
-            <nav>
-                <div className="menu">
-                    <ul>
-                        <li>
-                            <NavLink onClick={close} activeClassName="current" to="/">
-                                Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink onClick={close} activeClassName="current" to="/register">
-                                Cadastre-se
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink onClick={close} activeClassName="current" to="/profile">
-                                Perfil
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink onClick={close} activeClassName="current" to="/userlist">
-                                Lista
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
+      <Popup
+        modal
+        overlayStyle={{ background: "rgb(59, 39, 55)" }}
+        contentStyle={containerModal}
+        closeOnDocumentClick={false}
+        trigger={(open) => <BurgerIcon open={open} />}
+      >
+        {(close) => <Menu close={close} />}
+        <nav>
+          <div className="menu">
+            <ul>
+              <li>
+                <NavLink onClick={close} activeClassName="current" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  onClick={close}
+                  activeClassName="current"
+                  to="/register"
+                >
+                  Cadastre-se
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  onClick={close}
+                  activeClassName="current"
+                  to="/profile"
+                >
+                  Perfil
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  onClick={close}
+                  activeClassName="current"
+                  to="/userlist"
+                >
+                  Lista
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </Popup>
     );
 }
 
