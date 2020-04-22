2becdcce49236dfcf1b30bd4596d0c9d0eba0263
lefsampaio <leticia.cosmetic2011@gmail.com>
2020-04-22 20:25:08 -0300

    queries

--- src/Pages/UserList/index.js
+++ src/Pages/UserList/index.js
@@ -2,39 +2,33 @@ import React from 'react';
 import Routes from '../../routes';
 import Menu from '../../components/Menu';
 import './styles.css';
-import { gql } from "apollo-boost";
-import { useQuery } from '@apollo/react-hooks';
-
-
-const USER_INFO = gql`
-  {
-    users {
-      id
-      name
-    }
-  }
-`;
+import { userQueries } from "./queries";
+import apollo from '../../service/apollo';
 
 const UserList = () => {
 
-    const { loading, error, data } = useQuery(USER_INFO);
+    apollo.query({
+        query: userQueries,
+        variables: {
+            user: {
 
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error</p>;
+            }
+        }
+    })
 
     return (
         <>
             <Menu>
                 <Routes />
             </Menu>
-            {data.users.map(({ id, name }) => (
+            {/* {data.users.map(({ id, name }) => (
                 <div key={id}>
                     <p>
                         {id}: {name}
                     </p>
                 </div>
 
-            ))};
+            ))}; */}
         </>
     )
 
---
+++ src/Pages/UserList/queries.js
@@ -0,0 +1,15 @@
+import gql from 'graphql-tag';
+
+export const userQueries = gql`
+query 
+allUsers(
+$user: UserInput!
+$paginator: PaginatorInput
+){
+allUsers(
+user: $user
+paginator: $paginator
+){
+  name
+}
+}`
