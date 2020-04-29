38fcc6561e5d948c5398cc475d98db62f02c86cd
mokasousa <moka.sousa@gmail.com>
2020-04-29 19:50:45 -0300

    deleting comments

--- src/Pages/UserMap/index.js
+++ src/Pages/UserMap/index.js
@@ -6,39 +6,6 @@ import { FaSearch } from "react-icons/fa";
 import './styles.css';
 import AllUsers from '../../context/AllUsersContext'
 
-// const agents = [
-//     {
-//       id:'794r94h94hg9uh493hg9h', 
-//       name:'Leo', 
-//       job: 'Ninja',
-//       skills: ["Agroecologia", "Animais","Ativismo alimentar","Ativismo PCD","Bem estar","Comunicação"],
-//       city: 'Brasília', 
-//       state: 'DF', 
-//       lat:-15.769786, 
-//       lng:-47.871361
-//     },
-//     {
-//       id:'794riug7rhg9uh493hg9h', 
-//       name:'Don', 
-//       job: 'Ninja',
-//       skills:["Cultura","Direito à Terra"], 
-//       city: 'Brasília', 
-//       state: 'DF', 
-//       lat:'-15.747978', 
-//       lng:'-47.871490'
-//     },
-//     {
-//       id:'794rojrngo99uh493hg9h', 
-//       name:'Raph', 
-//       job: 'Ninja',
-//       skills: ["Direitos a Cidade","Direitos Humanos","Economia"], 
-//       city: 'Brasília', 
-//       state: 'DF', 
-//       lat:'-15.766605', 
-//       lng:'-47.861877'
-//     }
-// ]
-
 const UserMap = () => {
   const [search, setSearch] = useState('');
   const {state} = useContext(AllUsers);
