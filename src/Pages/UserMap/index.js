d9f539571ce2e4c05710e9a34cd9a90ad44b9edd
lefsampaio <leticia.cosmetic2011@gmail.com>
2020-04-30 14:13:00 -0300

    ajuste no css do input no mapa e foto do agente

--- src/Pages/UserMap/index.js
+++ src/Pages/UserMap/index.js
@@ -8,29 +8,35 @@ import AllUsers from '../../context/AllUsersContext'
 
 const UserMap = () => {
   const [search, setSearch] = useState('');
-  const {state} = useContext(AllUsers);
+  const { state } = useContext(AllUsers);
 
   const initMap = useCallback(() => {
     const map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat:-23.543095, lng:-46.627235},
+      center: { lat: -23.543095, lng: -46.627235 },
       zoom: 12,
       mapTypeControl: false,
       streetViewControl: false
     });
-    
+
     let infoWindow = new window.google.maps.InfoWindow();
     state.map(agent => {
       const marker = new window.google.maps.Marker({
-        position: {lat:agent.address.latitude, lng:agent.address.longitude}, 
+        position: { lat: agent.address.latitude, lng: agent.address.longitude },
         icon: pin,
         map: map
       });
       return marker.addListener('click', () => {
-        const skill = agent.skills.map((skill,index) => `<div class='agent-skills-item' id=${index}>${skill}</div>`).join('');
+        const skill = agent.skills.map((skill, index) => `<div class='agent-skills-item' id=${index}>${skill}</div>`).join('');
         infoWindow.setContent(
-        `<div class='info-window'>
-          <div class='agent-info'>
-            <div class='agent-info--img'></div>
+          `<div class='info-window'>
+             <div class='agent-info'>
+               <div class='userinfo'>
+                 <img
+                  class='user-info--img'
+                  alt="user-photo"
+                  src=${agent.profile_image.mimified}
+                 >
+         </div>
             <div>
               <p class='agent-info--text1'>${agent.name}</p>
               <p class='agent-info--text2'>${agent.address.city} / ${agent.address.state}</p>
@@ -40,11 +46,11 @@ const UserMap = () => {
           <div class='agent-skills'>${skill}</div>
           <a href="#" class='agent-plus'>Ver Mais</a>
         </div>`
-      )
+        )
         return infoWindow.open(map, marker)
       })
     });
-  },[state])
+  }, [state])
 
   const loadMap = useCallback((url) => {
     const scripts = window.document.getElementsByTagName('script')[0]
@@ -60,20 +66,20 @@ const UserMap = () => {
     window.initMap = initMap;
   }, [initMap, loadMap])
 
-    useEffect(() => {
-        renderMap();
-    }, [renderMap]);
+  useEffect(() => {
+    renderMap();
+  }, [renderMap]);
 
   return (
     <div className='map-container'>
       <div className='map-input'>
         <div className='input-container'>
-          <input 
+          <input
             name='search'
             value={search}
-            onChange={(e) => setSearch(e.target.value)} 
-            type='text' 
-            placeholder='Procurar...' 
+            onChange={(e) => setSearch(e.target.value)}
+            type='text'
+            placeholder='Procurar...'
             id='search'
             className='input-container--search'
           />
@@ -82,10 +88,10 @@ const UserMap = () => {
       </div>
       <div className="map-toggles">
         <Link to="/mapa">
-            <ToggleButton className="btn-toggle-map--blue">Mapa</ToggleButton>
+          <ToggleButton className="btn-toggle-map--blue">Mapa</ToggleButton>
         </Link>
         <Link to="/userlist">
-            <ToggleButton className="btn-toggle-map">Lista</ToggleButton>
+          <ToggleButton className="btn-toggle-map">Lista</ToggleButton>
         </Link>
       </div>
       <div id='map' className='map-display'></div>
