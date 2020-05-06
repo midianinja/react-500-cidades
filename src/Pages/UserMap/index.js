4c00127a443055057b774deca94eb6704bb2557e
mokasousa <moka.sousa@gmail.com>
2020-05-06 16:21:52 -0300

    update bounds

--- src/Pages/UserMap/index.js
+++ src/Pages/UserMap/index.js
@@ -3,19 +3,26 @@ import { Link } from 'react-router-dom';
 import ToggleButton from '../../components/ToggleButton';
 import pin from '../../assets/marcador-oportunidade.svg';
 import { FaSearch } from "react-icons/fa";
-import './styles.css';
 import AllUsers from '../../context/AllUsersContext';
 import NavigationBar from '../../components/NavigationBar';
+import './styles.css';
 
 const UserMap = () => {
   const { state } = useContext(AllUsers);
   const searchInputRef = useRef(null);
   const mapRef = useRef(null);
+  const brazilBounds = {
+    north:-73.9872354804, south:-33.7683777809, west:-34.7299934555, east:5.24448639569
+  }
 
   const initMap = useCallback(() => {
     mapRef.current = new window.google.maps.Map(document.getElementById('map'), {
       center: { lat: -23.543095, lng: -46.627235 },
       zoom: 12,
+      restriction:{
+        latLngBounds: brazilBounds,
+        strictBounds: false
+      },
       mapTypeControl: false,
       streetViewControl: false
     });
@@ -54,7 +61,7 @@ const UserMap = () => {
     });
 
     const autocomplete = new window.google.maps.places.Autocomplete(searchInputRef.current);
-    // autocomplete.bindTo('bounds', map);
+    autocomplete.bindTo('bounds', mapRef.current);
     autocomplete.setFields(['address_components', 'geometry', 'icon', 'name']);
 
     autocomplete.addListener('place_changed', () => {
@@ -62,15 +69,15 @@ const UserMap = () => {
       if (!place.geometry) {
         window.alert(`Não foi possível encontrar ${place.name}`);
         return;
-     // }
-     // if (place.geometry.viewport) {
-     //   map.fitBounds(place.geometry.viewport);
+      }
+      if (place.geometry.viewport) {
+        mapRef.current.fitBounds(place.geometry.viewport);
       } else {
         mapRef.current.setCenter(place.geometry.location);
         mapRef.current.setZoom(17);
       }
     });
-  }, [state])
+  }, [state, brazilBounds])
 
   const loadMap = useCallback((url) => {
     const scripts = window.document.getElementsByTagName('script')[0]
