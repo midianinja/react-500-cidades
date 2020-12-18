c000ead909bade81ddbc7cc5d7b7d8ddcc2e8310
Solange Nalao <solange.nalao@e-deploy.com.br>
2020-12-18 14:38:37 -0300

    feat: Correcoes de texto.

--- src/Pages/Register/register.model.js
+++ src/Pages/Register/register.model.js
@@ -75,7 +75,7 @@ export default {
         "Agronomia",
         "Alimentos",
         "Animação",
-        "Aquicultura",
+        "Agricultura",
         "Arqueologia",
         "Arquitetura e Urbanismo",
         "Arquivologia",
