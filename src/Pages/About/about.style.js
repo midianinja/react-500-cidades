da0d1d18c8d6e5e3382044cb0af07f96f59a74a7
Solange Nalao <solange.nalao@e-deploy.com.br>
2021-01-05 13:53:32 -0300

    feat:Inclusao de paginas de texto legal.

--- src/Pages/About/about.style.js
+++ src/Pages/About/about.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import aboutImage from '../../assets/banner-sobre.png';
 
 export const Container = styled.section`
     background-color: var(--off-white);
@@ -6,14 +7,33 @@ export const Container = styled.section`
     min-height: 100vh;
 `;
 
+export const BannerImg = styled.div`
+    width:100%;
+    height: 40vh;
+    background: url(${aboutImage}) var(--eggplant);
+    background-position: 50% -40px;
+    background-repeat: no-repeat;
+    background-attachment: scroll;
+    background-size: cover;
+    h1 {
+        position: absolute;
+        top: 25%;
+        left: 25%;
+        font-size: 2.6rem;
+        font-weight: 700;
+        line-height: 3.1rem;
+        color: var(--white);
+    }
+`;
+
 export const Wrapper = styled.article`
     height: auto;
     min-height: 100vh;
     width: 80%;
-    margin: 2.5rem auto;
+    margin: 0 auto;
     padding: 5rem 9rem;
     background-color: var(--white);
-    border-radius:10px;
+    border-radius: 0 0 10px 10px;
     font-family: Roboto;
     font-style: normal;
     color: #222;
@@ -22,13 +42,11 @@ export const Wrapper = styled.article`
         text-align:center;
         margin-top: 3rem;
     }
+    p:nth-last-child(-n + 3) {
+            width:50%;
+        }
 `;
 
-export const Title =  styled.h1`
-    font-size: 2.6rem;
-    font-weight: bold;
-    line-height: 3.1rem;
-    margin-bottom:2.5rem;
-`;
+
 
 
--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-    Title,
+    BannerImg,
     Wrapper,
     Container
   } from './about.style';
@@ -14,15 +14,16 @@ const About = ({history}) => {
         <Container>
         <NavigationBar />
         <Menu />
+        <BannerImg>
+            <h1>Conheça o 500 Cidades</h1>
+        </BannerImg>
         <Wrapper>
-            <Title>Termos de Uso</Title>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temporary incididunt ut labore et dolore magna aliqua. Enim adim ad minimim veniam, quis nostrud exercise ullamco laboris nisi ut alipip ex ea commodo consequat. Duis auteur irure pain in reprehenderit in voluptate velit this cillum dolore eu fugiat nulla pariatur. Excepteur sint occecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
            <div>
             <Button onClick={() => history.push('/')} className="btn3D--blue">Voltar</Button>
-            </div>
-                
+            </div>     
         </Wrapper>
         </Container>
     );
